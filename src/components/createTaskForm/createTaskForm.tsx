a29802cfd2c8b85e7d94611065776a4d3b7c888b
zm2666 <83871614+zm2666@users.noreply.github.com>
2023-02-28 14:57:05 +0100

    Created task select field

--- src/components/createTaskForm/createTaskForm.tsx
+++ src/components/createTaskForm/createTaskForm.tsx
@@ -3,6 +3,9 @@ import { Box, Typography, Stack } from '@mui/material';
 import { TaskTitleField } from './_taskTitleField';
 import { TaskDescriptionField } from './_taskDescriptionField';
 import { TaskDateField } from './_taskDateField';
+import { TaskSelectField } from './_taskSelectField';
+import { Status } from './enums/Status';
+import { Priority } from './enums/Priority.ts';
 
 export const CreateTaskForm: FC = (): ReactElement => {
     return (
@@ -22,6 +25,44 @@ export const CreateTaskForm: FC = (): ReactElement => {
                 <TaskTitleField />
                 <TaskDescriptionField />
                 <TaskDateField />
+                <Stack
+                    sx={{ width: '100%' }}
+                    direction="row"
+                    spacing={2}
+                >
+                    <TaskSelectField
+                        label="Status"
+                        name="status"
+                        items={[
+                            {
+                                value: Status.todo,
+                                label: Status.todo.toUpperCase(),
+                            },
+                            {
+                                value: Status.inProgress,
+                                label: Status.inProgress.toUpperCase(),
+                            },
+                        ]}
+                    />
+                    <TaskSelectField
+                        label="Priority"
+                        name="priority"
+                        items={[
+                            {
+                                value: Priority.low,
+                                label: Priority.low,
+                            },
+                            {
+                                value: Priority.normal,
+                                label: Priority.normal,
+                            },
+                            {
+                                value: Priority.high,
+                                label: Priority.high,
+                            },
+                        ]}
+                    />
+                </Stack>
             </Stack>
         </Box>
     );
