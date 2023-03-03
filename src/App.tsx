d9f80ae8714da63720d4ff5ad7d9f21a0454d814
zm2666 <83871614+zm2666@users.noreply.github.com>
2023-03-03 16:51:26 +0100

    Added tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import { ThemeProvider, CssBaseline } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
 import { customTheme } from './theme/customTheme.ts';
 import { Dashboard } from './pages/dashboard/dashboard.tsx';
+import {
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+//Create a client
+const queryClient = new QueryClient();
 
 const App: FC = (): ReactElement => {
     return (
-        <ThemeProvider theme={customTheme}>
-            <CssBaseline />
-            <Dashboard />
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider theme={customTheme}>
+                <CssBaseline />
+                <Dashboard />
+            </ThemeProvider>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     );
 };
 
